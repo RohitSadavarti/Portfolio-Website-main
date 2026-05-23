8b37f2e2fd67efce466ebeb169c1ded8132e07d2
Rohit Sadavarti <rsadavarti2@gmail.com>
2026-05-23 13:37:27 +0530

    Update vite.config.ts

--- Portfolio-Website-main/vite.config.ts
+++ Portfolio-Website-main/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-refresh' // or your current react plugin
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/Portfolio-Website-main/', // Add your exact repository name here wrapped in slashes
+})
