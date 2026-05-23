ab25aef5c85f0f1cad24250305253a40e68aa391
Rohit Sadavarti <rsadavarti2@gmail.com>
2026-05-23 13:34:08 +0530

    Update TechStack.tsx

--- Portfolio-Website-main/src/components/TechStack.tsx
+++ Portfolio-Website-main/src/components/TechStack.tsx
@@ -13,14 +13,12 @@ import {
 
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
-  "/images/react2.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
-  "/images/typescript.webp",
-  "/images/javascript.webp",
+  "/images/powerbi.svg",
+  "/images/sqlserver.svg",
+  "/images/python.svg",
+  "/images/excel.svg",
+  "/images/tableau.svg",
+  "/images/fabric.svg"
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
