8253de427d479573e23b4cd357d78d56935be25d
Rohit Sadavarti <rsadavarti2@gmail.com>
2026-05-24 02:47:25 +0530

    Update Contact.tsx

--- Portfolio-Website-main/src/components/Contact.tsx
+++ Portfolio-Website-main/src/components/Contact.tsx
@@ -24,7 +24,7 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com"
+              href="https://github.com/RohitSadavarti/"
               target="_blank"
               data-cursor="disable"
               className="contact-social"
